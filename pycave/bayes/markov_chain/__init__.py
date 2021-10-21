81570df40914a156a21417f644bf2ccfa936c378
Oliver Borchert <oliver@taskfleet.io>
2021-10-21 23:26:22 +0200

    Remove superfluous imports

--- pycave/bayes/markov_chain/__init__.py
+++ pycave/bayes/markov_chain/__init__.py
@@ -1,10 +1,8 @@
 from .estimator import MarkovChain
-from .lightning_module import MarkovChainLightningModule
 from .model import MarkovChainModel, MarkovChainModelConfig
 
 __all__ = [
     "MarkovChain",
-    "MarkovChainLightningModule",
     "MarkovChainModel",
     "MarkovChainModelConfig",
 ]
--- pycave/clustering/kmeans/__init__.py
+++ pycave/clustering/kmeans/__init__.py
@@ -1,10 +1,8 @@
 from .estimator import KMeans
-from .lightning_module import KMeansLightningModule
 from .model import KMeansModel, KMeansModelConfig
 
 __all__ = [
     "KMeans",
-    "KMeansLightningModule",
     "KMeansModel",
     "KMeansModelConfig",
 ]
