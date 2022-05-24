a5e7e4b2f97cb7fb8f87918d40a617ef5ea447e1
Oliver Borchert <me@borchero.com>
2022-05-24 03:31:45 +0200

    Try to increase the chance of flaky tests to complete

--- tests/bayes/gmm/test_gmm_estimator.py
+++ tests/bayes/gmm/test_gmm_estimator.py
@@ -50,7 +50,7 @@ def test_fit_converged(batch_size: Optional[int], max_epochs: int, converged: bo
     assert estimator.converged_ == converged
 
 
-@pytest.mark.flaky(max_runs=10, min_passes=1)
+@pytest.mark.flaky(max_runs=25, min_passes=1)
 @pytest.mark.parametrize(
     ("num_datapoints", "batch_size", "num_features", "num_components", "covariance_type"),
     [
